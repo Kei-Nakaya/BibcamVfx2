36a550b9c859546a93a3177dbeccedaaeeab2549
KN <kei.n.mp4@gmail.com>
2022-08-13 13:03:27 +0900

    データコミット/gitignore内の「*.mp4」記述削除
    Thumbs.db
    Desktop.ini
    .DS_Store
    *.swp
    
    /Library
    /Logs
    /Recordings
    /Temp
    /UserSettings
    
    *.mp4　←このmp4記述を削除

---
+++ Assets/Drums/DemoScripts/DrumsProxyCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DrumsProxyCollider : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _clip;
+    [SerializeField]
+    private AudioSource _source;
+    [SerializeField]
+    private float _soundDelay;
+    [SerializeField]
+    private UnityEvent OnClick;
+
+    private void OnMouseUpAsButton()
+    {
+        if (_source && _clip)
+        {
+            Invoke(nameof(PlaySound), _soundDelay);
+        }
+        
+        OnClick?.Invoke();
+    }
+
+    private void PlaySound()
+    {
+        _source.PlayOneShot(_clip);
+    }
+}
